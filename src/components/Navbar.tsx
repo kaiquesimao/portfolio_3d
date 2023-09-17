00663a26b2463769315fa11797a7389e852bc00d
kaiquesimao <kaique.gabriel.me@gmail.com>
2023-09-17 14:58:40 -0300

    FEAT: More Navbar configs

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ import { navLinks } from '../constants'
 import { logo, menu, close } from '../assets'
 
 const Navbar = () => {
-    const [, setActive] = useState('')
+    const [active, setActive] = useState('')
     return (
         <nav
             className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
@@ -28,7 +28,29 @@ const Navbar = () => {
                         alt={'logo'}
                         className={'w-9 h-9 object-contain'}
                     />
+                    <p
+                        className={
+                            'text-white text-[18px] font-bold cursor-pointer'
+                        }
+                    >
+                        Kaique
+                        <span className={'sm:block hidden'}>| Portfolio</span>
+                    </p>
                 </Link>
+                <ul className={'list-none hidden sm:flex flex-row gap-10'}>
+                    {navLinks.map((link) => (
+                        <li
+                            key={link.id}
+                            className={`${
+                                active === link.title
+                                    ? 'text-white'
+                                    : 'text-secondary'
+                            }`}
+                        >
+                            <a href={`#${link.id}`}>{link.title}</a>
+                        </li>
+                    ))}
+                </ul>
             </div>
         </nav>
     )
