eb0d1513abc82bfd8c909aec1fa7dcfe554e3bbf
kaiquesimao <kaique.gabriel.me@gmail.com>
2024-05-14 19:07:24 -0300

    CHORE: Removed the not used canvasLoader labels

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,20 +1,9 @@
-import { Html, useProgress } from "@react-three/drei";
+import { Html } from "@react-three/drei";
 
 const Loader = () => {
-  const { progress } = useProgress();
   return (
     <Html>
-      <div className={"canvas-loader"}>
-        <p
-          style={{
-            fontSize: 14,
-            color: "white",
-            fontWeight: 800,
-          }}
-        >
-          Loading...{progress.toFixed(2)}%
-        </p>
-      </div>
+      <div className={"canvas-loader"}></div>
     </Html>
   );
 };
