5395da623e2f151d189e9bf286875e1013361437
kaiquesimao <kaique.gabriel.me@gmail.com>
2024-05-27 15:39:58 -0300

    FIX: Navbar not hidden on mobile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,16 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { styles } from "../styles";
 import { languages, navLinks } from "../constants";
 import { logo, menu, close, brasil, usa } from "../assets";
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
+import MobileContext from "../contexts/MobileContext.tsx";
 
 const Navbar = () => {
   const { t, i18n } = useTranslation();
   const [active, setActive] = useState("");
   const [toggle, setToggle] = useState(true);
+  const isMobile = useContext(MobileContext);
 
   const handleLanguageChange = async (language: {
     nativeName: string;
@@ -46,115 +48,123 @@ const Navbar = () => {
             {t("page_title")}
           </p>
         </Link>
-        <ul className={"hidden list-none flex-row gap-10 sm:flex"}>
-          {navLinks.map((link) => (
-            <li
-              key={link.id}
-              className={`${
-                active === link.id ? "text-white" : "text-secondary"
-              } cursor-pointer text-lg font-medium hover:text-white`}
-              onClickCapture={() => setActive(link.id)}
-            >
-              <a href={`#${link.id}`}>{t(link.title)}</a>
-            </li>
-          ))}
-          <img
-            src={brasil}
-            alt={t("country_img")}
-            className={countryImgStyleClass(languages.portuguese.code)}
-            title={t("change_language")}
-            onClick={() => {
-              const handle = async () => {
-                await handleLanguageChange(languages.portuguese);
-              };
-              void handle();
-            }}
-          />
-          <img
-            src={usa}
-            alt={t("country_img")}
-            className={countryImgStyleClass(languages.english.code)}
-            title={t("change_language")}
-            onClick={() => {
-              const handle = async () => {
-                await handleLanguageChange(languages.english);
-              };
-              void handle();
-            }}
-          />
-        </ul>
-        <div className={"flex flex-1 items-center justify-end sm:hidden"}>
-          <div className="relative">
+
+        {/* desktop */}
+        {!isMobile && (
+          <ul className={"hidden list-none flex-row gap-10 sm:flex"}>
+            {navLinks.map((link) => (
+              <li
+                key={link.id}
+                className={`${
+                  active === link.id ? "text-white" : "text-secondary"
+                } cursor-pointer text-lg font-medium hover:text-white`}
+                onClickCapture={() => setActive(link.id)}
+              >
+                <a href={`#${link.id}`}>{t(link.title)}</a>
+              </li>
+            ))}
             <img
-              src={menu}
-              alt={"menu"}
-              className={`absolute transition-opacity duration-500 ${
-                toggle ? "opacity-100" : "opacity-0"
-              } size-7 cursor-pointer object-contain`}
-              onClickCapture={() => setToggle(!toggle)}
+              src={brasil}
+              alt={t("country_img")}
+              className={countryImgStyleClass(languages.portuguese.code)}
+              title={t("change_language")}
+              onClick={() => {
+                const handle = async () => {
+                  await handleLanguageChange(languages.portuguese);
+                };
+                void handle();
+              }}
             />
             <img
-              src={close}
-              alt={"close"}
+              src={usa}
+              alt={t("country_img")}
+              className={countryImgStyleClass(languages.english.code)}
+              title={t("change_language")}
+              onClick={() => {
+                const handle = async () => {
+                  await handleLanguageChange(languages.english);
+                };
+                void handle();
+              }}
+            />
+          </ul>
+        )}
+
+        {/* mobile */}
+        {isMobile && (
+          <div className={"flex flex-1 items-center justify-end sm:hidden"}>
+            <div className="relative">
+              <img
+                src={menu}
+                alt={"menu"}
+                className={`absolute transition-opacity duration-500 ${
+                  toggle ? "opacity-100" : "opacity-0"
+                } size-7 cursor-pointer object-contain`}
+                onClickCapture={() => setToggle(!toggle)}
+              />
+              <img
+                src={close}
+                alt={"close"}
+                className={`transition-opacity duration-500 ${
+                  toggle ? "opacity-0" : "opacity-100"
+                } size-7 cursor-pointer object-contain`}
+                onClickCapture={() => setToggle(!toggle)}
+              />
+            </div>
+            <div
               className={`transition-opacity duration-500 ${
                 toggle ? "opacity-0" : "opacity-100"
-              } size-7 cursor-pointer object-contain`}
-              onClickCapture={() => setToggle(!toggle)}
-            />
-          </div>
-          <div
-            className={`transition-opacity duration-500 ${
-              toggle ? "opacity-0" : "opacity-100"
-            } black-gradient absolute right-0 top-20 z-10 mx-4 my-2 min-w-36 rounded-xl p-6`}
-          >
-            <ul
-              className={
-                "flex list-none flex-col items-start justify-end gap-4"
-              }
+              } black-gradient absolute right-0 top-20 z-10 mx-4 my-2 min-w-36 rounded-xl p-6 ${toggle ? "hidden" : ""}`}
             >
-              {navLinks.map((link) => (
-                <li
-                  key={link.id}
-                  className={`${
-                    active === link.id ? "text-white" : "text-secondary"
-                  } cursor-pointer font-[Poppins] text-base font-medium`}
-                  onClickCapture={() => {
-                    setToggle(!toggle);
-                    setActive(link.id);
-                  }}
-                >
-                  <a href={`#${link.id}`}>{t(link.title)}</a>
-                </li>
-              ))}
-              <div className={"flex flex-row justify-between gap-4"}>
-                <img
-                  src={brasil}
-                  alt={t("country_img")}
-                  className={countryImgStyleClass(languages.portuguese.code)}
-                  title={t("change_language")}
-                  onClick={() => {
-                    const handle = async () => {
-                      await handleLanguageChange(languages.portuguese);
-                    };
-                    void handle();
-                  }}
-                />
-                <img
-                  src={usa}
-                  alt={t("country_img")}
-                  className={countryImgStyleClass(languages.english.code)}
-                  title={t("change_language")}
-                  onClick={() => {
-                    const handle = async () => {
-                      await handleLanguageChange(languages.english);
-                    };
-                    void handle();
-                  }}
-                />
-              </div>
-            </ul>
+              <ul
+                className={
+                  "flex list-none flex-col items-start justify-end gap-4"
+                }
+              >
+                {navLinks.map((link) => (
+                  <li
+                    key={link.id}
+                    className={`${
+                      active === link.id ? "text-white" : "text-secondary"
+                    } cursor-pointer font-[Poppins] text-base font-medium`}
+                    onClickCapture={() => {
+                      setToggle(!toggle);
+                      setActive(link.id);
+                    }}
+                  >
+                    <a href={`#${link.id}`}>{t(link.title)}</a>
+                  </li>
+                ))}
+                <div className={"flex flex-row justify-between gap-4"}>
+                  <img
+                    src={brasil}
+                    alt={t("country_img")}
+                    className={countryImgStyleClass(languages.portuguese.code)}
+                    title={t("change_language")}
+                    onClick={() => {
+                      const handle = async () => {
+                        await handleLanguageChange(languages.portuguese);
+                      };
+                      void handle();
+                    }}
+                  />
+                  <img
+                    src={usa}
+                    alt={t("country_img")}
+                    className={countryImgStyleClass(languages.english.code)}
+                    title={t("change_language")}
+                    onClick={() => {
+                      const handle = async () => {
+                        await handleLanguageChange(languages.english);
+                      };
+                      void handle();
+                    }}
+                  />
+                </div>
+              </ul>
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </nav>
   );
